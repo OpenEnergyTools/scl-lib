51be9396c7ec34924378c531544827759ec55ae8
Jakob Vogelsang <jakob-vogelsang@posteo.de>
2023-10-10 16:40:43 +0200

    feat(tIED): updateIED

---
+++ tIED/updateIED.spec.ts
@@ -0,0 +1,135 @@
+import { expect } from "chai";
+
+import { handleEdit } from "@openscd/open-scd-core";
+
+import { Edit, isUpdate } from "../foundation/utils.js";
+
+import { scl } from "./updateIED.testfile.js";
+
+import { updateIED } from "./updateIED.js";
+
+function numberUpdates(edits: Edit[], tag: string): number {
+  return edits.filter((edit) => isUpdate(edit) && edit.element.tagName === tag)
+    .length;
+}
+
+const pub = new DOMParser()
+  .parseFromString(scl, "application/xml")
+  .querySelector('IED[name="Pub"]')!;
+
+const publi = new DOMParser()
+  .parseFromString(scl, "application/xml")
+  .querySelector('IED[name="Publi"]')!;
+
+const client = new DOMParser()
+  .parseFromString(scl, "application/xml")
+  .querySelector('IED[name="Client"]')!;
+
+const substation = new DOMParser()
+  .parseFromString(scl, "application/xml")
+  .querySelector("Substation")!;
+
+describe("Function to an update the IED name attributes and its referenced elements", () => {
+  it("returns empty array with non IED update", () =>
+    expect(updateIED({ element: substation, attributes: {} }).length).to.equal(
+      0
+    ));
+
+  it("returns empty array with missing old IED name", () => {
+    const sclDom = new DOMParser().parseFromString(scl, "application/xml");
+    const publi = sclDom.querySelector('IED[name="Publi"]')!;
+    publi.removeAttribute("name");
+
+    expect(
+      updateIED({ element: publi, attributes: { name: "newIedName" } }).length
+    ).to.equal(0);
+  });
+
+  it("returns no additional edits when name attribute is not updated", () =>
+    expect(
+      updateIED({ element: pub, attributes: { desc: "newDesc" } }).length
+    ).to.equal(1));
+
+  it("updates LNode iedName attributes as well", () => {
+    const edits = updateIED({ element: pub, attributes: { name: "newPub" } });
+
+    expect(numberUpdates(edits, "LNode")).to.equal(2);
+  });
+
+  it("updates ConnectedAP iedName attributes as well", () => {
+    const edits = updateIED({ element: pub, attributes: { name: "newPub" } });
+
+    expect(numberUpdates(edits, "ConnectedAP")).to.equal(1);
+  });
+
+  it("updates ExtRef iedName attributes as well", () => {
+    const edits = updateIED({ element: pub, attributes: { name: "newPub" } });
+
+    expect(numberUpdates(edits, "ExtRef")).to.equal(8);
+  });
+
+  it("updates KDC iedName attributes as well", () => {
+    const edits = updateIED({ element: pub, attributes: { name: "newPub" } });
+
+    expect(numberUpdates(edits, "KDC")).to.equal(1);
+  });
+
+  it("updates Association iedName attributes as well", () => {
+    const edits = updateIED({ element: publi, attributes: { name: "newPub" } });
+
+    expect(numberUpdates(edits, "Association")).to.equal(1);
+  });
+
+  it("updates ClientLN iedName attributes as well", () => {
+    const edits = updateIED({
+      element: client,
+      attributes: { name: "newIED" },
+    });
+
+    expect(numberUpdates(edits, "ClientLN")).to.equal(2);
+  });
+
+  it("updates IEDName content as well", () => {
+    const sclDom = new DOMParser().parseFromString(scl, "application/xml");
+    const subscriber1 = sclDom.querySelector('IED[name="Subscriber1"]')!;
+
+    const before = Array.from(sclDom.querySelectorAll("IEDName")).filter(
+      (iedName) => iedName.textContent === "newIedName"
+    );
+    expect(before.length).to.equal(0);
+
+    const edits = updateIED({
+      element: subscriber1,
+      attributes: { name: "newIedName" },
+    });
+
+    handleEdit(edits);
+
+    const after = Array.from(sclDom.querySelectorAll("IEDName")).filter(
+      (iedName) => iedName.textContent === "newIedName"
+    );
+    expect(after.length).to.equal(8);
+  });
+
+  it("updates LGOS/LSVS object reference", () => {
+    const sclDom = new DOMParser().parseFromString(scl, "application/xml");
+    const publi = sclDom.querySelector('IED[name="Publi"]')!;
+
+    const before = Array.from(
+      sclDom.querySelectorAll('LN[lnClass="LGOS"] Val, LN[lnClass="LSVS"] Val')
+    ).filter((iedName) => iedName.textContent?.startsWith("newIedName"));
+    expect(before.length).to.equal(0);
+
+    const edits = updateIED({
+      element: publi,
+      attributes: { name: "newIedName" },
+    });
+
+    handleEdit(edits);
+
+    const after = Array.from(
+      sclDom.querySelectorAll('LN[lnClass="LGOS"] Val, LN[lnClass="LSVS"] Val')
+    ).filter((iedName) => iedName.textContent?.startsWith("newIedName"));
+    expect(after.length).to.equal(4);
+  });
+});
---
+++ tIED/updateIED.ts
@@ -0,0 +1,165 @@
+import { Edit, Update } from "../foundation/utils.js";
+
+import { controlBlockObjRef } from "../tControl/controlBlockObjRef.js";
+
+function isPublic(element: Element): boolean {
+  return !element.closest("Private");
+}
+
+const elementsWithIedNameAttribute = [
+  "LNode",
+  "ConnectedAP",
+  "KDC",
+  "ExtRef",
+  "ClientLN",
+  "Association",
+];
+
+function updateIEDNameTextContent(
+  ied: Element,
+  oldIedName: string,
+  newIedName: string
+): Edit[] {
+  return Array.from(ied.ownerDocument.getElementsByTagName("IEDName"))
+    .filter(isPublic)
+    .filter((iedName) => iedName.textContent === oldIedName)
+    .map((iedName) => {
+      const node = Array.from(iedName.childNodes).find(
+        (node) => node.nodeType === Node.TEXT_NODE
+      )!;
+
+      return [
+        { node },
+        { parent: iedName, node: document.createTextNode(newIedName) },
+      ];
+    });
+}
+
+/** Valid is:
+ * 1. there is an control block in the IED with the name change (ied)
+ * 2. this control block is subscribed in otherIED (iedName, srcLDInst and srcCBName match)
+ * 3. there is a LGOS/LSVS > ... > setSrcRef holding this control block object reference
+ */
+function validSubscriptionSupervision(
+  ied: Element,
+  otherIED: Element,
+  oldIedName: string
+): Element[] {
+  // for GSEControl elements
+  const lgosVals = Array.from(ied.querySelectorAll("GSEControl"))
+    .filter((srcCB) => {
+      //filter out all control blocks that are not subscribed in otherIED
+      const srcLDInst = srcCB.closest("LDevice")?.getAttribute("inst");
+      return !!otherIED.querySelector(
+        `:scope > AccessPoint > Server > LDevice 
+        ExtRef[iedName="${oldIedName}"][srcLDInst="${srcLDInst}"][srcCBName="${srcCB.getAttribute(
+          "name"
+        )}"]`
+      );
+    })
+    .map((srcCB) => {
+      const objRef = controlBlockObjRef(srcCB);
+      return Array.from(
+        otherIED.querySelectorAll(
+          `:scope > AccessPoint > Server > LDevice > LN[lnClass="LGOS"] > 
+            DOI[name="GoCBRef"] > DAI[name="setSrcRef"] > Val`
+        )
+      ).find((val) => val.textContent === objRef);
+    })
+    .filter((val) => val) as Element[];
+
+  // for SampledValueControl elements
+  const lsvsVals = Array.from(ied.querySelectorAll("SampledValueControl"))
+    .filter((srcCB) => {
+      //filter out all control blocks that are not subscribed in otherIED
+      const srcLDInst = srcCB.closest("LDevice")?.getAttribute("inst");
+      return !!otherIED.querySelector(
+        `:scope > AccessPoint > Server > LDevice 
+          ExtRef[iedName="${oldIedName}"][srcLDInst="${srcLDInst}"][srcCBName="${srcCB.getAttribute(
+          "name"
+        )}"]`
+      );
+    })
+    .map((srcCB) => {
+      const objRef = controlBlockObjRef(srcCB);
+      return Array.from(
+        otherIED.querySelectorAll(
+          `:scope > AccessPoint > Server > LDevice > LN[lnClass="LSVS"] > 
+          DOI[name="SvCBRef"] > DAI[name="setSrcRef"] > Val`
+        )
+      ).find((val) => val.textContent === objRef);
+    })
+    .filter((val) => val) as Element[];
+
+  return [...lgosVals, ...lsvsVals];
+}
+
+function updateSubscriptionSupervision(
+  ied: Element,
+  oldIedName: string,
+  newIedName: string
+): Edit[] {
+  const vals = Array.from(
+    ied.ownerDocument.querySelectorAll(":root > IED")
+  ).flatMap((otherIED) =>
+    validSubscriptionSupervision(ied, otherIED, oldIedName)
+  );
+
+  return vals.flatMap((val) => {
+    const oldContent = val.textContent!;
+    const newContent = oldContent.replace(oldIedName, newIedName);
+    const newTextNode = document.createTextNode(newContent);
+
+    const node = Array.from(val.childNodes).find(
+      (childNode) => childNode.nodeType === Node.TEXT_NODE
+    )!;
+
+    return [{ node }, { parent: val, node: newTextNode }];
+  });
+}
+
+function updateIedNameAttributes(
+  ied: Element,
+  oldIedName: string,
+  newIedName: string
+): Update[] {
+  const selector = elementsWithIedNameAttribute
+    .map((iedNameElement) => `${iedNameElement}[iedName="${oldIedName}"]`)
+    .join(",");
+
+  return Array.from(ied.ownerDocument.querySelectorAll(selector))
+    .filter(isPublic)
+    .map((element) => {
+      return { element, attributes: { iedName: newIedName } };
+    });
+}
+
+/**
+ * Function to schema valid update name and other attribute(s) in IED element
+ * (rename IED)
+ * ```md
+ * The function makes sure to also
+ * 1. Update all elements with iedName attribute referenced to the IED.name
+ *    attribute such as LNode, ClientLN, ExtRef, KDC, Association, ConnectedAP
+ * 2. Update all control block object references pointing to the IED
+ * 3. Updates IEDName elements text content
+ * ```
+ * @param update - IED element and attributes to be changed in the IED element
+ * @returns - Set of addition edits updating all references SCL elements
+ */
+export function updateIED(update: Update): Edit[] {
+  if (update.element.tagName !== "IED") return [];
+  if (!update.attributes.name) return [update];
+
+  const ied = update.element;
+  const oldIedName = ied.getAttribute("name");
+  const newIedName = update.attributes.name!;
+  if (!oldIedName) return [];
+
+  return [
+    update,
+    ...updateIedNameAttributes(ied, oldIedName, newIedName),
+    ...updateSubscriptionSupervision(ied, oldIedName, newIedName),
+    ...updateIEDNameTextContent(ied, oldIedName, newIedName),
+  ];
+}
